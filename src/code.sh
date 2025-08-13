59aa3a5f8915d76d779816eb19b56f0580173bb4
newmanreb <rebekah.s.newman@gmail.com>
2025-08-13 16:10:58 +0100

    attempt 6 at fixing merge

--- src/code.sh
+++ src/code.sh
@@ -154,7 +154,9 @@ function merge_vcfs {
     # Old merge command: 
     #docker run -v /:/data "${BCFTOOLS_DOCKER_IMAGE_NAME}" merge -O z -o /data/${PWD}/${1}_merged.vcf.gz /data/${PWD}/*.vcf.gz 
     
-    docker run -v /home/dnanexus:/data "${BCFTOOLS_DOCKER_IMAGE_NAME}" merge -m all -O z -f -o /data/${PWD}/"${1}_merged.vcf.gz" /data/*.vcf.gz
+    #docker run -v /home/dnanexus:/data "${BCFTOOLS_DOCKER_IMAGE_NAME}" merge -m all -O z -f -o /data/${PWD}/"${1}_merged.vcf.gz" /data/*.vcf.gz
+
+    docker run -v /:/data "${BCFTOOLS_DOCKER_IMAGE_NAME}" merge -m all -O z -f -o /data${PWD}/"${1}_merged.vcf.gz" /data${PWD}/*.vcf.gz
 
     # Use bcftools v1.6 docker container to create an index of the merged VCF file, which is required by Peddy.
     # The -t flag indexes the file using tabix.
