8e409fadd9e947cbf96179a75eb6611d58ea4b11
newmanreb <rebekah.s.newman@gmail.com>
2025-10-20 15:50:34 +0100

    Fixed versions of bcftools mentioned in in-line comments

--- src/code.sh
+++ src/code.sh
@@ -21,7 +21,7 @@ dx download ${BCFTOOLS_DOCKER_FILE_ID}
 BCFTOOLS_DOCKER_IMAGE_FILE=$(dx describe ${BCFTOOLS_DOCKER_FILE_ID} --name)
 BCFTOOLS_DOCKER_IMAGE_NAME=$(tar xfO "${BCFTOOLS_DOCKER_IMAGE_FILE}" manifest.json | sed -E 's/.*"RepoTags":\["?([^"]*)"?.*/\1/')
 
-# Call bcftools view (Bcftools v1.13). Docker image is a DNAnexus asset in 001_ToolsReferenceData in compressed
+# Call bcftools view (Bcftools v1.15). Docker image is a DNAnexus asset in 001_ToolsReferenceData in compressed
 # tarball format.
 docker load < /home/dnanexus/"${BCFTOOLS_DOCKER_IMAGE_FILE}"
 echo "Using docker image ${BCFTOOLS_DOCKER_IMAGE_NAME}"
@@ -68,7 +68,7 @@ function rename_vcf_header {
     # Write VCF filename to temp file.
     echo ${new_sample_name} > $tmp
 
-    # Use bcftools v1.6 docker container to rename the sample name in vcf header, using the sample name in the temp file.
+    # Use bcftools v1.15 docker container to rename the sample name in vcf header, using the sample name in the temp file.
     # By default `bcftools reheader` writes the edited VCF to the console, but here the result is
     # redirected to the file temp.$vcf_file.
     # -v /:/data mounts the root of the dnanexus worker to /data within the docker container to allow file access
@@ -78,7 +78,7 @@ function rename_vcf_header {
     # Rename edited VCF using the name of the original VCF, deleting the original in the process.
     mv temp.$vcf_file $vcf_file
 
-    # Use bcftools v1.6 docker container to create an index for the updated vcf file which will be required by
+    # Use bcftools v1.15 docker container to create an index for the updated vcf file which will be required by
     # `bcftools merge`. The -t flag indexes the file using tabix.
     # -v /:/data mounts the root of the dnanexus worker to /data within the docker container to allow file access
     docker run -v /:/data "${BCFTOOLS_DOCKER_IMAGE_NAME}" index -t /data/${PWD}/$vcf_file
