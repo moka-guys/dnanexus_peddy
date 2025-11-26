ef311bf7523a3d79ef37a2e0b376b959d261fcc4
newmanreb <rebekah.s.newman@gmail.com>
2025-11-26 17:27:43 +0000

    Updating peddy sites to 1million rather than 100k as data is still lost.

--- src/code.sh
+++ src/code.sh
@@ -187,7 +187,7 @@ merge_vcfs "${project_for_peddy}"
 ############### Loading in pre-built Docker image of Peddy ###############
 
 # Download docker image, get tag and print
-PEDDY_DOCKER_FILE_ID=project-J0yfKqQ0yyqvgGfB3jxgk5Xg:file-J4YXpv80yyqX43FkZK6qYZQk
+PEDDY_DOCKER_FILE_ID=project-J0yfKqQ0yyqvgGfB3jxgk5Xg:file-J4YYPp00yyqv1vKzVpyv98y2
 dx download ${PEDDY_DOCKER_FILE_ID} -o ped_peddy.tar.gz
 
 # Use the safe filename
